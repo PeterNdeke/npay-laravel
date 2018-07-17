66d5e3d351130e9af32d13af87eea4570a452080
PeterNdeke <35370838+PeterNdeke@users.noreply.github.com>
2018-07-17 13:58:15 +0100

    second commit

---
+++ scr/NpayServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is for npay laravel integartion for developers.
+ *
+ * (c)peterNdeke <ndekepeter2015@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace numericscoder\Npay;
+
+use Illuminate\Support\ServiceProvider;
+
+class NpayServiceProvider extends ServiceProvider
+{
+
+    /*
+    * Indicates if loading of the provider is deferred.
+    *
+    * @var bool
+    */
+    protected $defer = false;
+
+    /**
+    * Publishes all the config file this package needs to function
+    */
+    public function boot()
+    {
+        $config = realpath(__DIR__.'/../resources/config/npay.php');
+
+        $this->publishes([
+            $config => config_path('npay.php')
+        ]);
+    }
+
+    /**
+    * Register the application services.
+    */
+    public function register()
+    {
+        $this->app->bind('npay-laravel', function () {
+
+            return new Npay;
+
+        });
+    }
+
+    /**
+    * Get the services provided by the provider
+    * @return array
+    */
+    public function provides()
+    {
+        return ['npay-laravel'];
+    }
+}
